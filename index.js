465a18e3fa6eff7a5c84354681dfe834970c7f1f
Ali Raza <aliraza@fastechsystem.co>
2023-09-08 18:37:49 +0500

    implement callback function into inside function

--- index.js
+++ index.js
@@ -12,11 +12,23 @@
 
 
 
+//given below prgram is executed by using callback to wait 
 console.log("Before");
+
+
+// getRepositories('ali-raza', function (repositories) {
+//     console.log('Repositories:', repositories);
+//   });
+  
 
 getUser(1,function(user){
     console.log("Ali Raza");
     console.log('User',user);
+
+    getRepositories(user.getRepositories,(repos) =>{
+        console.log('Repos',repos)
+    });
+
 });
 console.log('After');
 
@@ -27,7 +39,14 @@ console.log('After');
 function getUser(id,callback){
         setTimeout(()=>{
             console.log('Reading a user from a database...');
-            callback({id: id, gitHubUserName: 'mosh'})
+            callback({id: id, gitHubUserName: 'ali'})
         },2000);
 }
 
+function getRepositories (username,callback){
+
+        setTimeout(()=>{
+            console.log('Calling GitHub API ..');
+            callback(['repo1','repo2','repo3']);
+        },3000);
+}
